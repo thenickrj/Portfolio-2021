217c649c9a1db82ce42edfe35827dcaa1bee7231
Nikhil Singh <nikhilbangalor@gmail.com>
2021-09-02 20:47:48 +0530

    commit

--- src/components/Test/Test.js
+++ src/components/Test/Test.js
@@ -2,80 +2,12 @@ import React from "react";
 import styled from "styled-components";
 
 const IntroText = styled.div`
-  display: flex;
-  background-color: #252839;
-  align-items: center;
-  justify-content: center;
-  min-height: 100vh;
-  color: #252839;
-
-  .loading {
-    position: relative;
-    font-size: 2vw;
-    color: #01fe87;
-    -webkit-text-stroke: 0.3vw #383d52;
-    text-transform: uppercase;
-  }
-
-  .loading::before {
-    content: attr(data-text);
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 0;
-    height: 100%;
-    color: #01fe87;
-    -webkit-text-stroke: 0vw #383d52;
-    border-right: 2px solid #01fe87;
-    overflow: hidden;
-    animation: animate 6s linear infinite;
-  }
-
-  @keyframes animate {
-    0%,
-    10%,
-    100% {
-      width: 0;
-    }
-    70%,
-    90% {
-      width: 100%;
-    }
-  }
-
-  #btc {
-    width: 150px;
-    height: 100px;
-    position: absolute;
-    right: 5%;
-    top: 5%;
-    background-image: url("	https://www.udemy.com/staticx/udemy/images/v/logo-coral.svg");
-    background-size: contain;
-    background-repeat: no-repeat;
-  }
-  @import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;600;800;900&display=swap");
-
-  * {
-    overflow-x: hidden !important;
-  }
   #cursor {
     overflow: hidden;
     position: fixed;
     top: 200px;
     left: 200px;
   }
-  html {
-    cursor: none;
-  }
-  body {
-    width: 100%;
-    height: 100vh;
-    overflow: hidden !important;
-    margin: 0;
-    padding: 0;
-    font-family: "Montserrat", sans-serif;
-  }
-
   #homeSection {
     width: 100%;
     height: 100vh;
@@ -168,7 +100,7 @@ const IntroText = styled.div`
   }
 `;
 
-function Intro() {
+function Test() {
   const gsap = window.gsap;
 
   let tl = gsap.timeline({ ease: "power1.in" });
@@ -208,69 +140,11 @@ function Intro() {
   let letters = document.querySelectorAll("#letter");
 
   letters.forEach((element) => {
-    element.addEventListener("mouseenter", () => {
-      gsap.to("#cursor", {
-        scale: 2,
-        onStart: () => {
-          gsap.to("#circ", { stroke: colors[randomColor] });
-        },
-      });
-      randomColor = Math.floor(Math.random() * colors.length);
-      randomSize = Math.floor(Math.random() * sizes.length);
-      gsap.to(element, {
-        color: colors[randomColor],
-        rotate: sizes[randomSize],
-        y: `${sizes[randomSize]}%`,
-        duration: 0.5,
-      });
-    });
-    element.addEventListener("mouseleave", () => {
-      gsap.to("#cursor", {
-        scale: 1,
-        onStart: () => {
-          gsap.to("#circ", { stroke: ogColor });
-        },
-      });
-      gsap.to(element, { y: "0%", color: ogColor, rotate: 0, duration: 0.5 });
-    });
-    element.addEventListener("touchstart", () => {
-      gsap.to("#cursor", {
-        scale: 2,
-        onStart: () => {
-          gsap.to("#circ", { stroke: colors[randomColor] });
-        },
-      });
-      randomColor = Math.floor(Math.random() * colors.length);
-      randomSize = Math.floor(Math.random() * sizes.length);
-      gsap.to(element, {
-        color: colors[randomColor],
-        rotate: sizes[randomSize],
-        y: `${sizes[randomSize]}%`,
-        duration: 0.5,
-      });
-    });
-    element.addEventListener("touchend", () => {
-      gsap.to("#cursor", {
-        scale: 1,
-        onStart: () => {
-          gsap.to("#circ", { stroke: ogColor });
-        },
-      });
-      gsap.to(element, { y: "0%", color: ogColor, rotate: 0, duration: 0.5 });
-    });
+    // element.addEventListener("mouseenter");
   });
-
   return (
     <div>
-      Hi! I'm Nikhil Singh and I'm a Full Stack Developer!
       <IntroText>
-        <h2 className="loading" data-text="Hi! I'm Nikhil Singh">
-          Hi! I'm Nikhil Singh and
-          <br /> I'm a Full Stack Developer!
-        </h2>
-        <h2 className="loading" data-text="I'm a Full Stack Developer!">
-          I'm a Full Stack Developer!
-        </h2>
         <svg id="cursor" height="50" width="50">
           <circle
             cx="20"
@@ -281,8 +155,6 @@ function Intro() {
             fill="transparent"
             id="circ"
           />
-          {/* <!-- coordinates are in x,y format --> */}
-          {/* <!-- points go clockwise around the polygon --> */}
         </svg>
         <div id="homeSection">
           <div id="textPosition" className="left">
@@ -381,4 +253,4 @@ function Intro() {
   );
 }
 
-export default Intro;
+export default Test;
